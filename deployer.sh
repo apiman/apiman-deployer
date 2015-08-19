9d4ccc8d6b33e76002671fa0bee104cecb6b6eee
Eric Wittmann <eric.wittmann@redhat.com>
2015-08-19 17:02:50 -0400

    added initial support for deploying the apiman gateway

--- deployer.sh
+++ deployer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+APIMAN_VERSION=1.1.6.Final
+
 echo "###############################################################"
 echo "# Welcome to the apiman deployer.  Use this utility to deploy #"
 echo "# a single apiman component onto a target system.             #"
@@ -13,8 +15,8 @@ echo ""
 echo "Currently supported components:"
 echo "    1. Elasticsearch (for metrics)"
 echo "    2. Keycloak Authentication Server"
-echo "    3. apiman: API Manager"
-echo "    4. apiman: API Gateway"
+echo "    3. apiman: API Gateway"
+echo "    4. apiman: API Manager"
 read -p "Which component would you like to deploy? " COMPONENT
 
 
@@ -36,7 +38,7 @@ then
   echo "# Installation complete. You can now start up Elasticsearch        #"
   echo "# with the following command:                                      #"
   echo "#                                                                  #"
-  echo "#    ~/apiman-elasticsearch/elasticsearch-1.7.1/bin/elasticsearch  #"
+  echo "    ~/apiman-elasticsearch/elasticsearch-1.7.1/bin/elasticsearch"
   echo "#                                                                  #"
   echo "# or add the -d option to start Elasticsearch in the background.   #"
   echo "#                                                                  #"
@@ -45,5 +47,54 @@ then
   echo "# network between apiman and Elasticsearch is not secure.          #"
   echo "####################################################################"
   
+fi
+
+
+
+if [ "x$COMPONENT" = "x2" ]
+then
+  echo "###############################################################"
+  echo "# (Keycloak) NOT YET SUPPORTED :(                             #"
+  echo "###############################################################"
+fi
+
+
+
+if [ "x$COMPONENT" = "x3" ]
+then
+  echo "###############################################################"
+  echo "# Installing apiman : API Gateway...                          #"
+  echo "###############################################################"
+  mkdir ~/apiman-gateway-$APIMAN_VERSION
+  cd ~/apiman-gateway-$APIMAN_VERSION
+  curl http://downloads.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.zip -o wildfly-8.2.0.Final.zip
+  curl http://downloads.jboss.org/apiman/1.1.6.Final/apiman-distro-wildfly8-1.1.6.Final-overlay.zip -o apiman-distro-wildfly8-1.1.6.Final-overlay.zip
+  unzip wildfly-8.2.0.Final.zip
+  unzip -o apiman-distro-wildfly8-1.1.6.Final-overlay.zip -d wildfly-8.2.0.Final
+  cd wildfly-8.2.0.Final
+  rm -f standalone/deployments/apiman-ds.xml
+  rm -f standalone/deployments/apiman-es.war
+  rm -f standalone/deployments/apiman.war
+  rm -f standalone/deployments/apimanui.war
+  sed -i "s/<enabled>true<\/enabled>/<enabled>false<\/enabled>/g" standalone/configuration/standalone-apiman.xml
+
+  echo "####################################################################"
+  echo "# Installation complete. You can now start up apiman : API Gateway #"
+  echo "# with the following commands:                                     #"
+  echo "#                                                                  #"
+  echo "    cd ~/apiman-gateway-$APIMAN_VERSION/wildfly-8.2.0.Final"
+  echo "    ./bin/standalone.sh -b 0.0.0.0 -c standalone-apiman.xml"
+  echo "#                                                                  #"
+  echo "####################################################################"
+
+fi
+
+
+
+if [ "x$COMPONENT" = "x4" ]
+then
+  echo "###############################################################"
+  echo "# (apiman: API Manager) NOT YET SUPPORTED :(                  #"
+  echo "###############################################################"
 fi
 
